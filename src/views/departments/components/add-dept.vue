02093e59688b9bc583abc4762ceb0617519a8187
顾小夕 <2504408987@qq.com>
2022-12-07 10:53:27 +0800

    新增部门规则效验|处理首页pid数据

--- src/views/departments/components/add-dept.vue
+++ src/views/departments/components/add-dept.vue
@@ -3,18 +3,18 @@
   <el-dialog title="新增部门" :visible="showDialog">
     <!-- 表单组件  el-form   label-width设置label的宽度   -->
     <!-- 匿名插槽 -->
-    <el-form label-width="120px">
-      <el-form-item label="部门名称">
-        <el-input style="width: 80%" placeholder="1-50个字符" />
+    <el-form label-width="120px" :model="formData" :rules="rules">
+      <el-form-item label="部门名称" prop="name">
+        <el-input v-model="formData.name" style="width: 80%" placeholder="1-50个字符" />
       </el-form-item>
-      <el-form-item label="部门编码">
-        <el-input style="width: 80%" placeholder="1-50个字符" />
+      <el-form-item label="部门编码" prop="code">
+        <el-input v-model="formData.code" style="width: 80%" placeholder="1-50个字符" />
       </el-form-item>
-      <el-form-item label="部门负责人">
-        <el-select style="width: 80%" placeholder="请选择" />
+      <el-form-item label="部门负责人" prop="manager">
+        <el-select v-model="formData.manager" style="width: 80%" placeholder="请选择" />
       </el-form-item>
-      <el-form-item label="部门介绍">
-        <el-input style="width: 80%" placeholder="1-300个字符" type="textarea" :rows="3" />
+      <el-form-item label="部门介绍" prop="introduce">
+        <el-input v-model="formData.introduce" style="width: 80%" placeholder="1-300个字符" type="textarea" :rows="3" />
       </el-form-item>
     </el-form>
     <!-- el-dialog有专门放置底部操作栏的 插槽  具名插槽 -->
@@ -29,11 +29,57 @@
 </template>
 
 <script>
+import { getDepartments } from '@/api/departments'
 export default {
   props: {
     showDialog: {
       type: Boolean,
       default: false
+    },
+    treeNode: {
+      type: Object,
+      default: null
+    }
+  },
+  data() {
+    const checkNameRepeat = async (rule, value, callback) => {
+      const { depts } = await getDepartments()
+      const isRepeat = depts.filter((item) => item.pid === this.treeNode.id).some((item) => item.name === value)
+      isRepeat ? callback(new Error(`同级部门下已有【${value}】的部门了`)) : callback()
+    }
+    const checkCodeRepeat = async (rule, value, callback) => {
+      const { depts } = await getDepartments()
+      const isRepeat = depts.some((item) => item.code === value && value)
+      isRepeat ? callback(new Error(`组织架构下已有【${value}】的编码了`)) : callback()
+    }
+    return {
+      formData: {
+        // 部门名称
+        name: '',
+        // 部门编码
+        code: '',
+        // 部门管理者
+        manager: '',
+        // 部门介绍
+        introduce: ''
+      },
+      rules: {
+        name: [
+          { required: true, message: '部门名称不能为空', trigger: 'blur' },
+          { min: 1, max: 50, message: '部门名称要求1-50个字符', trigger: 'blur' },
+          { trigger: 'blur', validator: checkNameRepeat }
+        ],
+        code: [
+          { required: true, message: '部门编码不能为空', trigger: 'blur' },
+          { min: 1, max: 50, message: '部门编码要求1-50个字符', trigger: 'blur' },
+          { trigger: 'blur', validator: checkCodeRepeat }
+        ],
+        manager: [{ required: true, message: '部门负责人不能为空', trigger: 'blur' }],
+        introduce: [
+          { required: true, message: '部门介绍不能为空', trigger: 'blur' },
+          { trigger: 'blur', min: 1, max: 300, message: '部门介绍要求1-50个字符' }
+        ]
+      }
     }
   }
 }
