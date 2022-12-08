341462f1c487648fc5699e1f220a9e8f34b9eeec
顾小夕 <2504408987@qq.com>
2022-12-08 15:25:52 +0800

    新增公司设置页面[待写交互]

---
+++ src/api/setting.js
@@ -0,0 +1,15 @@
+import request from '@/utils/request'
+// 获取角色信息
+export function getRoleList(params) {
+  return request({
+    url: '/sys/role',
+    params
+  })
+}
+
+// 获取公司信息
+export function getCompanyInfo(companyId) {
+  return request({
+    url: `/company/${companyId}`
+  })
+}
--- src/views/setting/index.vue
+++ src/views/setting/index.vue
@@ -1,13 +1,87 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <h2>公司设置</h2>
+      <el-card>
+        <el-tabs>
+          <!-- 放置页签 -->
+          <el-tab-pane label="角色管理">
+            <!-- 新增角色按钮 -->
+            <el-row style="height: 60px">
+              <el-button icon="el-icon-plus" size="small" type="primary">新增角色</el-button>
+            </el-row>
+            <!-- 表格 -->
+            <el-table border="" :data="list">
+              <el-table-column align="center" type="index" label="序号" width="120" />
+              <el-table-column align="center" prop="name" label="名称" width="240" />
+              <el-table-column align="center" prop="description" label="描述" />
+              <el-table-column align="center" label="操作">
+                <el-button size="small" type="success">分配权限</el-button>
+                <el-button size="small" type="primary">编辑</el-button>
+                <el-button size="small" type="danger">删除</el-button>
+              </el-table-column>
+            </el-table>
+
+            <!-- 分页组件 -->
+            <el-row type="flex" justify="center" align="middle" style="height: 60px">
+              <el-pagination :current-page="page.page" :page-size="page.pagesize" :total="page.total" layout="prev, pager, next" @current-change="changePage" />
+            </el-row>
+          </el-tab-pane>
+          <el-tab-pane label="公司信息">
+            <el-alert title="对公司名称、公司地址、营业执照、公司地区的更新，将使得公司资料被重新审核，请谨慎修改" type="info" show-icon :closable="false" />
+            <el-form label-width="120px" style="margin-top: 50px">
+              <el-form-item label="公司名称">
+                <el-input disabled style="width: 400px" />
+              </el-form-item>
+              <el-form-item label="公司地址">
+                <el-input disabled style="width: 400px" />
+              </el-form-item>
+              <el-form-item label="邮箱">
+                <el-input disabled style="width: 400px" />
+              </el-form-item>
+              <el-form-item label="备注">
+                <el-input type="textarea" :rows="3" disabled style="width: 400px" />
+              </el-form-item>
+            </el-form>
+          </el-tab-pane>
+        </el-tabs>
+      </el-card>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+import { getRoleList } from '@/api/setting'
+export default {
+  data() {
+    return {
+      list: [],
+      page: {
+        // 页数
+        page: 1,
+        // 一页几条
+        pagesize: 10,
+        // 记录总数
+        total: 0
+      }
+    }
+  },
+  created() {
+    this.getRoleList()
+  },
+  methods: {
+    // 获取数据
+    async getRoleList() {
+      const { total, rows } = await getRoleList(this.page)
+      this.page.total = total
+      this.list = rows
+    },
+    // 触发事件
+    changePage(newPage) {
+      this.page.page = newPage
+      this.getRoleList()
+    }
+  }
+}
 </script>
 
 <style></style>
