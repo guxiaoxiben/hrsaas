a04053d9e1a882d98fe0338c83f21f70bd942e18
sansui <2504408987@qq.com>
2022-12-11 09:23:17 +0800

    修改添加显示bug

--- src/api/constant/employees.js
+++ src/api/constant/employees.js
@@ -3,11 +3,11 @@ export default {
   // 聘用形式
   hireType: [
     {
-      id: 1,
+      id: '1',
       value: '正式'
     },
     {
-      id: 2,
+      id: '2',
       value: '非正式'
     }
   ],
@@ -417,4 +417,3 @@ export default {
     }
   ]
 }
-
