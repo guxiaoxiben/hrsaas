89d30f5e332fbe9439af278c69285c2f603d66e2
sansui <2504408987@qq.com>
2022-12-27 11:35:54 +0800

    新增tab页的视图

--- src/components/index.js
+++ src/components/index.js
@@ -5,6 +5,7 @@ import Print from 'vue-print-nb'
 import ScreenFull from './ScreenFull'
 import ThemePicker from './ThemePicker'
 import lang from './lang'
+import TagsView from './TagsView'
 export default {
   install(Vue) {
     Vue.component('PageTools', PageTools) // 注册工具栏组件
@@ -13,6 +14,7 @@ export default {
     Vue.component('ScreenFull', ScreenFull) // 注册全屏组件
     Vue.component('ThemePicker', ThemePicker) // 注册动态主题组件
     Vue.component('lang', lang) // 注册多语言组件
+    Vue.component('TagsView', TagsView) // 注册多页组件
 
     Vue.use(Print) // 注册打印组件
   }
